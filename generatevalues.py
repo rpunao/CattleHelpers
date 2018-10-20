2334f673e770b4592bda0886a172e8849170045f
unknown <reypunao@yahoo.com>
2018-10-19 20:16:48 -0700

    Altered data to seem more realistic

--- generatevalues.py
+++ generatevalues.py
@@ -1,10 +1,29 @@
 import random
 from datetime import datetime
 
+CONST_LAT = 35.325467
+CONST_LON = -120.653275
+CONST_DELTA = 0.002500
+CONST_TIME = datetime.now()
+
+def setNewLat(old):
+    return old + round(random.uniform(-CONST_DELTA*2, CONST_DELTA*2), 6)
+
+def setNewLon(old):
+    return old + round(random.uniform(-CONST_DELTA*2, CONST_DELTA*2), 6)
+
+def newLat(currLat):
+    delt = round(random.uniform(-CONST_DELTA, CONST_DELTA), 6)
+    return CONST_LAT + delt
+
+def newLon(currLon):
+    delt = round(random.uniform(-CONST_DELTA, CONST_DELTA), 6)
+    return CONST_LON + delt
+
 def randLat():
     CONST_LAT1 = 35.323659
     CONST_LAT2 = 35.3227741
-    lat = round(random.uniform(CONST_LAT2, CONST_LAT1),6)
+    lat = round(random.uniform(CONST_LAT2, CONST_LAT1), 6)
     return str(lat)
 
 def randLon():
@@ -16,4 +35,6 @@ def randLon():
 def getTime():
     dt = datetime.now()
     dt.isoformat("T")
-    return str(dt)
+    return dt
+
+print(newLat(0))
--- insertdata.py
+++ insertdata.py
@@ -1,7 +1,11 @@
 import psycopg2
 import generatevalues as value
 
-NUM_RECORDS = 10
+NUM_RECORDS = 30
+CONST_LAT = 35.325467
+CONST_LON = -120.653275
+from datetime import datetime
+from datetime import timedelta
 
 def postgres_insert():
     try:
@@ -9,17 +13,22 @@ def postgres_insert():
             "dbname='CattleDB' user='cattleroot' host='cattleinstance.coo92izzstxf.us-west-2.rds.amazonaws.com' password='05t*XQDxuuaM'")
         cur = conn.cursor()
 
+        time_delta = 0
         for i in range(NUM_RECORDS):
-            time = value.getTime()
-            lon = value.randLon()
-            lat = value.randLat()
+            if (i % 7 == 0):
+                CONST_LAT = value.setNewLat(CONST_LAT)
+                CONST_LON = value.setNewLon(CONST_LON)
+            time = str(value.getTime() + timedelta(minutes=time_delta))
+            lon = str(value.newLon(CONST_LON))
+            lat = str(value.newLat(CONST_LAT))
             device_id = '1'
             farm_id = '1'
             command = ('INSERT INTO location (date, longitude, latitude, device_id_id, farm_id_id) VALUES (\''
                        + time + '\', ' + lon + ', ' + lat + ', ' + device_id + ', ' + farm_id + ');')
-            cur.execute(command)
-            conn.commit()
 
+            time_delta = time_delta + 15
+        cur.execute(command)
+        conn.commit()
         conn.close()
         return True
     except:
